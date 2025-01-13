678fed64825b14c2a72ef6e77719137ffffa4ee5
Pratik Singh <pratikkumars623@gmail.com>
2025-01-13 14:56:36 +0530

    Job Listing Pages created

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -1,9 +1,25 @@
 import supabaseClient from "@/utils/supabase";
 
-export async function getJobs(token)
+export async function getJobs(token,{location,company_id,searchQuery})
 {
     const supabase= await supabaseClient(token);
-    let query=supabase.from("jobs").select("*")
+    let query=supabase.from("jobs").select("*,company:companies(name,logo_url),saved:saved_jobs(id)");
+
+    if(location)
+    {
+        query=query.eq("location",location);
+    }
+
+    if(company_id)
+    {
+            query=query.eq("company_id",company_id);
+    }
+
+    if(searchQuery)
+    {
+        query=query.ilike("title",`%${searchQuery}%`);
+    }
+    
     const { data, error }=await query
     if(error)
     {
@@ -12,3 +28,36 @@ export async function getJobs(token)
     }
     return data;
 }
+
+// - Add / Remove Saved Job
+export async function saveJob(token, { alreadySaved }, saveData) {
+    const supabase = await supabaseClient(token);
+  
+    if (alreadySaved) {
+      // If the job is already saved, remove it
+      const { data, error: deleteError } = await supabase
+        .from("saved_jobs")
+        .delete()
+        .eq("job_id", saveData.job_id);
+  
+      if (deleteError) {
+        console.error("Error removing saved job:", deleteError);
+        return data;
+      }
+  
+      return data;
+    } else {
+      // If the job is not saved, add it to saved jobs
+      const { data, error: insertError } = await supabase
+        .from("saved_jobs")
+        .insert([saveData])
+        .select();
+  
+      if (insertError) {
+        console.error("Error saving job:", insertError);
+        return data;
+      }
+  
+      return data;
+    }
+  }
--- src/pages/JobListing.jsx
+++ src/pages/JobListing.jsx
@@ -1,22 +1,62 @@
+import { useEffect, useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+import { BarLoader } from "react-spinners";
+
 import { getJobs } from "@/api/apiJobs";
-import { useSession } from "@clerk/clerk-react";
-import { useEffect } from "react";
-const JobListing=()=> {
+import useFetch from "@/hooks/UseFetch";
+import JobCard from "@/components/job-card";
 
-  const {session}=useSession()
-  const fetchJobs=async()=>{
-    const supabaseAccessToken=await session.getToken(
-    {
-      template:"supabase",
-    });
-    const data =await getJobs(supabaseAccessToken);
-    console.log(data);
+const JobListing = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState("");
+  const [company_id, setCompany_id] = useState("");
+
+  const { isLoaded } = useUser();
+
+  const {
+    data: jobs,
+    fn: fnJobs,
+    loading: loadingJobs,
+  } = useFetch(getJobs, {
+    location,
+    company_id,
+    searchQuery,
+  });
+
+  useEffect(() => {
+    if (isLoaded) fnJobs();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoaded, location, company_id, searchQuery]);
+
+  if (!isLoaded) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
   }
-  useEffect(()=>{
-    fetchJobs();
-  },[])
 
-  return <div>Job Listing</div>;
-}
+  return (
+    <div>
+      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
+        Latest Jobs
+      </h1>
+
+      {loadingJobs && (
+        <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />
+      )}
+
+      {loadingJobs===false && (
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {jobs?.length ? (
+            jobs.map((job) => {
+                return <JobCard key={job.id} job={job}
+                  savedInit={JobCard.saved?.length>0}
+                />
+            })
+          ) : (
+            <div>No jobs found!!!</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default JobListing;
